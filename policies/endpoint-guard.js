d926a888b22952803632e7c7de5f868806408636
Carlos Herrera <43161662+carlos-herrera-cervantes@users.noreply.github.com>
2022-10-29 01:36:51 -0500

    HVR-34 Added service endpoints

--- policies/endpoint-guard.js
+++ policies/endpoint-guard.js
@@ -3,10 +3,15 @@
 module.exports = {
   name: 'endpoint-guard',
   policy: (actionParams) => (req, res, next) => {
+    const rolesInHeaders = req.headers['user-roles'].split(',');
+    const roleMatch = actionParams.roles.split(',').some(element => rolesInHeaders.includes(element));
+
     for (const param of actionParams.endpoints) {
       const regex = new RegExp(param.regex);
+      const pathMatch = req.path.match(regex);
+      const methodMismatch = !param.methods.includes(req.method);
       
-      if (req.path.match(regex) && !param.methods.includes(req.method)) {
+      if (pathMatch && methodMismatch && roleMatch) {
         return res.status(403).send({ message: 'Invalid role' });
       }
     }
